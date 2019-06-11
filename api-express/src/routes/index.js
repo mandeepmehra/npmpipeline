98a95ffc382d1235e5c0f8f61eebc513bd907dc5
Priyanku Konar <konar.priyanku@gmail.com>
2019-06-12 01:34:36 +0530

    updated mongohost url

--- api-express/src/routes/index.js
+++ api-express/src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.send("Welcome to Express")
+  res.send("Welcome to xebia version 1.0.0")
 });
 
 export default router;
