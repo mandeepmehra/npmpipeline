30445b325bac6cbd366caf63ae9befc67a65b5f2
Priyanku Konar <konar.priyanku@gmail.com>
2019-06-12 17:24:18 +0530

    updated app  version to 21

--- api-express/src/routes/index.js
+++ api-express/src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.send("Welcome to xebia version 1.0.0")
+  res.send("Welcome to xebia version 21")
 });
 
 export default router;
